f515b91a7685ad08e9523e55e936b224ac908b70
bhavin_avdevs <bhavin@avdevs.com>
2016-05-31 18:39:36 +0530

    - HMC-18 - Flexible file names when serving images

--- controller.php
+++ controller.php
@@ -2,35 +2,28 @@
 namespace Concrete\Package\MykSeoPath;
 use Package;
 use Route;
-use Request;
-use \Doctrine\Common\Annotations\AnnotationRegistry;
 
 class Controller extends Package {
-
     protected $pkgHandle = 'myk_seo_path';
     protected $appVersionRequired = '5.7.1';
     protected $pkgVersion = '0.0.1';
-
     public function getPackageDescription() {
         return t("Package for SEO friendly path");
     }
-
     public function getPackageName() {
         return t(" SEO friendly path for files");
     }
-
     public function install() {
         $pkg = parent::install();
     }
-
     public function upgrade() {
         $pkg = parent::upgrade();
     }
-
     public function on_start() {
         Route::register('/files/{fID}/{keywords}', function ($fID, $keywords) {
             $file = \File::getByID($fID);
-            return $file->getURL();
+            $fh = \Core::make('helper/file');
+            echo $fh->getContents($file->getURL());
         });
     }
 }
