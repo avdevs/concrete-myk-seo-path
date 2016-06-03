dbdbec37ac2845714496556f07741ec03e7c3dca
bhavin_avdevs <bhavin@avdevs.com>
2016-06-03 17:53:25 +0530

    - Update for getallheaders() create issue on nginx

--- controller.php
+++ controller.php
@@ -8,10 +8,10 @@ class Controller extends Package {
     protected $appVersionRequired = '5.7.1';
     protected $pkgVersion = '0.0.1';
     public function getPackageDescription() {
-        return t("Package for SEO friendly path");
+        return t('Package for SEO friendly path');
     }
     public function getPackageName() {
-        return t(" SEO friendly path for files");
+        return t('SEO friendly path for files');
     }
     public function install() {
         $pkg = parent::install();
@@ -25,6 +25,25 @@ class Controller extends Package {
             if($file) {
                 $fh = \Core::make('helper/file');
                 $path = DIR_FILES_UPLOADED_STANDARD . '/' . $file->getFileResource()->getPath();
+
+                //getallheaders() create issue on nginx
+                //A request header of Owner-ID is going to be passed to php as $_SERVER['HTTP_OWNER_ID'] by nginx
+                if (!function_exists('getallheaders'))
+                {
+                    function getallheaders()
+                    {
+                        $headers = '';
+                        foreach ($_SERVER as $name => $value)
+                        {
+                            if (substr($name, 0, 5) == 'HTTP_')
+                            {
+                                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+                            }
+                        }
+                        return $headers;
+                    }
+                }
+
                 $headers = getallheaders();
                 if (isset($headers['If-Modified-Since']) && (strtotime($headers['If-Modified-Since']) == filemtime($path))) {
                     header('HTTP/1.0 304 Not Modified');
