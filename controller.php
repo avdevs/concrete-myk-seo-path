3504f429c53bb6fb0dbb22463ea80f70876c278d
bhavin_avdevs <bhavin@avdevs.com>
2016-06-03 15:58:21 +0530

    -Minor Updates

--- controller.php
+++ controller.php
@@ -27,13 +27,13 @@ class Controller extends Package {
                 $path = DIR_FILES_UPLOADED_STANDARD . '/' . $file->getFileResource()->getPath();
                 $headers = getallheaders();
                 if (isset($headers['If-Modified-Since']) && (strtotime($headers['If-Modified-Since']) == filemtime($path))) {
-                    header("HTTP/1.0 304 Not Modified");
+                    header('HTTP/1.0 304 Not Modified');
                     exit;
                 }
-                header( 'Content-Type: '.$file->getMimeType() );
-                header("Cache-Control: public");
-                header("Last-Modified: ". gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
-                header("Expires:" . date("r",time() + (60*60*24*30))); // Date in the past
+                header('Content-Type: '.$file->getMimeType());
+                header('Cache-Control: public');
+                header('Last-Modified: '. gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
+                header('Expires:' . date('r',time() + (60*60*24*30))); // Date in the past
                 echo $fh->getContents($path);
             }
         });
