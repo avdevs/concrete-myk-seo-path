6bb4e54b168ddf3823e41712439465e187c63e25
bhavin_avdevs <bhavin@avdevs.com>
2016-06-06 12:07:03 +0530

    - Get headers using Request::getInstance()

--- controller.php
+++ controller.php
@@ -2,6 +2,7 @@
 namespace Concrete\Package\MykSeoPath;
 use Package;
 use Route;
+use Request;
 
 class Controller extends Package {
     protected $pkgHandle = 'myk_seo_path';
@@ -26,34 +27,21 @@ class Controller extends Package {
                 $fh = \Core::make('helper/file');
                 $path = DIR_FILES_UPLOADED_STANDARD . '/' . $file->getFileResource()->getPath();
 
-                //getallheaders() create issue on nginx
-                //A request header of Owner-ID is going to be passed to php as $_SERVER['HTTP_OWNER_ID'] by nginx
-                if (!function_exists('getallheaders'))
-                {
-                    function getallheaders()
-                    {
-                        $headers = '';
-                        foreach ($_SERVER as $name => $value)
-                        {
-                            if (substr($name, 0, 5) == 'HTTP_')
-                            {
-                                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
-                            }
-                        }
-                        return $headers;
-                    }
-                }
-
-                $headers = getallheaders();
-                if (isset($headers['If-Modified-Since']) && (strtotime($headers['If-Modified-Since']) == filemtime($path))) {
+                $r = Request::getInstance();
+                $ifModifiedSince = $r->headers->get('if-modified-since');
+                if(isset($ifModifiedSince) && (strtotime($ifModifiedSince) == filemtime($path))) {
                     header('HTTP/1.0 304 Not Modified');
                     exit;
                 }
+                header('Pragma: cache');
                 header('Content-Type: '.$file->getMimeType());
-                header('Cache-Control: public');
+                header('Cache-Control: cache');
                 header('Last-Modified: '. gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
                 header('Expires:' . date('r',time() + (60*60*24*30))); // Date in the past
                 echo $fh->getContents($path);
+            }else{
+                header('HTTP/1.0 404 Not found');
+                exit;
             }
         });
     }
