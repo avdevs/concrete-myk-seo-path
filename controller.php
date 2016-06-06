d39440fbf8e166e8bc9c64f125d94ddcce26418f
bhavin_avdevs <bhavin@avdevs.com>
2016-06-06 14:26:16 +0530

    add GMT in expired time

--- controller.php
+++ controller.php
@@ -27,7 +27,7 @@ class Controller extends Package {
                 $fh = \Core::make('helper/file');
                 $path = DIR_FILES_UPLOADED_STANDARD . '/' . $file->getFileResource()->getPath();
 
-                $r = Request::getInstance();
+                    $r = Request::getInstance();
                 $ifModifiedSince = $r->headers->get('if-modified-since');
                 if(isset($ifModifiedSince) && (strtotime($ifModifiedSince) == filemtime($path))) {
                     header('HTTP/1.0 304 Not Modified');
@@ -37,7 +37,7 @@ class Controller extends Package {
                 header('Content-Type: '.$file->getMimeType());
                 header('Cache-Control: cache');
                 header('Last-Modified: '. gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
-                header('Expires:' . date('r',time() + (60*60*24*30))); // Date in the past
+                header('Expires:' . date('D, d M Y H:i:s',time() + (60*60*24*30)).' GMT'); // Date in the past
                 echo $fh->getContents($path);
             }else{
                 header('HTTP/1.0 404 Not found');
