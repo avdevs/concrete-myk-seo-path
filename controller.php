75b81a4e1b4511e70644b0935afc4283d3c30e5c
Dhaval Patel <dhaval@avdevs.com>
2018-09-14 10:09:35 +0530

    Cleared Output buffer, if it has any value.

--- controller.php
+++ controller.php
@@ -1,50 +1,67 @@
 <?php
 namespace Concrete\Package\MykSeoPath;
+
 use Package;
 use Route;
 use Request;
 use Concrete\Core\Http\FlysystemFileResponse;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
-class Controller extends Package {
+class Controller extends Package
+{
     protected $pkgHandle = 'myk_seo_path';
     protected $appVersionRequired = '5.7.1';
-    protected $pkgVersion = '0.0.1';
-    public function getPackageDescription() {
+    protected $pkgVersion = '0.0.2';
+
+    public function getPackageDescription()
+    {
         return t('Package for SEO friendly path');
     }
-    public function getPackageName() {
+
+    public function getPackageName()
+    {
         return t('SEO friendly path for files');
     }
-    public function install() {
+
+    public function install()
+    {
         $pkg = parent::install();
     }
-    public function upgrade() {
+
+    public function upgrade()
+    {
         $pkg = parent::upgrade();
     }
-    public function on_start() {
+
+    public function on_start()
+    {
+
+        if (ob_get_length() > 0) {
+            ob_end_clean();
+        }
+
         Route::register('/files/{fID}/{keywords}', function ($fID, $keywords) {
             $file = \File::getByID($fID);
-            if($file) {
+            if ($file) {
 
                 $fre = $file->getFileResource();
                 $path = DIR_FILES_UPLOADED_STANDARD . '/' . $fre->getPath();
                 $r = Request::getInstance();
                 $ifModifiedSince = $r->headers->get('if-modified-since');
-                if(isset($ifModifiedSince) && (strtotime($ifModifiedSince) == filemtime($path))) {
+                if (isset($ifModifiedSince) && (strtotime($ifModifiedSince) == filemtime($path))) {
                     header('HTTP/1.0 304 Not Modified');
                     exit;
                 }
                 $fs = $file->getFile()->getFileStorageLocationObject()->getFileSystemObject();
                 $response = new FlysystemFileResponse($fre->getPath(), $fs);
-                $response->headers->set('Cache-Control','cache');
-                $response->headers->set('Last-Modified',gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
-                $response->headers->set('Expires',date('D, d M Y H:i:s',time() + (60*60*24*30)).' GMT');
-                $response->headers->set('Pragma','cache');
+                $response->headers->set('Cache-Control', 'cache');
+                $response->headers->set('Last-Modified', gmdate('D, d M Y H:i:s', filemtime($path)) . ' GMT');
+                $response->headers->set('Expires', date('D, d M Y H:i:s', time() + (60 * 60 * 24 * 30)) . ' GMT');
+                $response->headers->set('Pragma', 'cache');
                 $response->prepare(\Request::getInstance());
                 return $response->send();
 
-            }else{
+            } else {
                 header('HTTP/1.0 404 Not found');
                 exit;
             }
@@ -53,11 +70,11 @@ class Controller extends Package {
         /* For Thumbnail */
         Route::register('/pictures/{fID}/{thumbnailHandle}/{keywords}', function ($fID, $thumbnailHandle, $keywords) {
             $file = \File::getByID($fID);
-            if($file) {
+            if ($file) {
 
                 $fre = $file->getFileResource();
                 $path = DIR_FILES_UPLOADED_STANDARD . '/thumbnails/' . $thumbnailHandle . '/' . $fre->getPath();
-                if(file_exists($path)) {
+                if (file_exists($path)) {
 
                     $r = Request::getInstance();
                     $ifModifiedSince = $r->headers->get('if-modified-since');
@@ -77,7 +94,7 @@ class Controller extends Package {
                     header('HTTP/1.0 404 Not found');
                     exit;
                 }
-            }else{
+            } else {
                 header('HTTP/1.0 404 Not found');
                 exit;
             }
